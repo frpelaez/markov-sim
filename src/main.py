switched to python3.14 and fixed distribution calculation bugs

src/main.py
@@ -1,27 +1,47 @@
 import numpy as np
 
 from simulation import Simulation
+from utils import matpow
 
 
 def main():
-    trials = 25
-    steps = 201
-    n_states = 51
+    trials = 3
+    steps = 101
+    n_states = 5
 
-    a = np.random.random(n_states)
-    a /= a.sum()
+    a = np.zeros((n_states,))
+    a[0] = 1.0
 
     t = np.zeros((n_states, n_states))
     t[0][1] = 1
     for i in range(1, n_states - 1):
-        t[i][i - 1] = 0.25
-        t[i][i] = 0.25
-        t[i][i + 1] = 0.5
-    t[-1][-1] = 1
+        t[i][i - 1] = 0.33
+        t[i][i] = 0.33
+        t[i][i + 1] = 0.34
+    t[-1][-2] = 1
 
-    sim = Simulation(trials, steps, n_states, a, t)
-    sim.run(verbose=False, show_plots=True)
-    # res = sim.result
+    sim = Simulation(trials, steps, n_states, a, t, estimate_distribution=True)
+    sim.run(verbose=False, show_plots=False)
+    res = sim.result
+
+    print("""
+          Estimations
+          """)
+    print("Final mat:\n", res.final_mat)
+    print("Final dist:\n", res.final_dist)
+    for i in range(n_states):
+        dist = sim.estimate_distribution_from_initial_state(i, steps)
+        print(f"Final dist from state {i}:\n", dist)
+
+    print("""
+          Exacts
+          """)
+    exact_mat = matpow(t, steps)
+    for i in range(exact_mat.shape[0]):
+        exact_mat[i] /= exact_mat[i].sum()
+    exact_dist = a @ exact_mat
+    print("Exact mat:\n", exact_mat)
+    print("Exact dist:\n", exact_dist)
 
 
 if __name__ == "__main__":

src/simulation.py
@@ -3,33 +3,33 @@ from dataclasses import dataclass
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-class ParameterError(Exception): ...
-
-
-class InvalidDistributionError(Exception): ...
-
+from errors import InvalidDistributionError, ParameterError
 
 DISTRIBUTION_TOLERANCE = 1e-9
 
 
 @dataclass
 class SimulationResultWrapper:
+    """
+    ### Object that encapsulates the results of a DTMC simulation.
+    """
+
     paths: list[list[int]]
-    estimated_final_distribution: np.ndarray | None = None
-    # TODO: añadir más métricas que permitan analizar la cadena
+    final_dist: np.ndarray | None = None
+    final_mat: np.ndarray | None = None
 
 
 class Simulation:
     """
-    Class to encapsulate a DTMC simulation. The user must provide the following parameters:
+    ### Class to encapsulate a DTMC simulation.
+    The user must provide the following parameters:
     - numer of trials for the simulation (int)
     - number of steps to run each trial (int)
     - number of states (int)
-    - the initial probability distribution
-    - the one-epoch transition matrix
+    - the initial probability distribution (ArrayLike)
+    - the one-epoch transition matrix (ArrayLike)
 
-    The `run` method of this class will populate an atribute called `result`. This is an object containing important information and metrics about the experiment. Some of the most important are `paths` (a lsit containing the generated trajectories) and `final_distribution` (probability distribution over the states given the transition matrix and the initial distribution).
+    The `run` method of this class will populate an atribute called `result`. This is an object containing important information and metrics about the experiment. The most important is `paths` (a list containing the generated trajectories).
     """
 
     def __init__(
@@ -39,6 +39,7 @@ class Simulation:
         n_states: int,
         initial_distribution: np.ndarray,
         transition_matrix: np.ndarray,
+        estimate_distribution: bool = False,
     ) -> None:
         self.trials = trials
         self.steps = steps
@@ -46,15 +47,17 @@ class Simulation:
         self.n_states = n_states
         self.initial_distribution = initial_distribution
         self.transition_matrix = transition_matrix
-        self.result: None | SimulationResultWrapper = None
+        self.result: SimulationResultWrapper = SimulationResultWrapper([])
+        self._est_dist = estimate_distribution
+        self._rng = np.random.RandomState(seed=12345)
 
     def run(self, verbose: bool = False, show_plots: bool = False) -> None:
         """
         Runs the simulation given the initialization parameters. Can print to the screen the simulation parameters, but this is not reccomended for larger experiments. Can also generate automathic graphics to illustrate the results of the simulation.
 
-        :Parameters:
-        -verbose (bool, optional) Show the initialization parameters. Defaults to False
-        -show_plots (bool, optional) Show the generated plots. Defaults to False
+        **Parameters**
+        - verbose (bool, optional) Show the initialization parameters. Defaults to False
+        - show_plots (bool, optional) Show the generated plots. Defaults to False
         """
         if not self._check_dimensions():
             msg = "Simulations parameters dimensions are incompatible."
@@ -66,15 +69,20 @@ class Simulation:
         if not self._check_valid_distributions():
             raise InvalidDistributionError(
                 "Either inital distribution or transition matrix are not valid."
-                + " They must represent, row ways, probability distributions, aka add up to 1"
+                + " They must represent, row wise, probability distributions, aka, add up to 1"
             )
 
         if verbose:
             self._print_parameters()
 
         paths = self._compute_paths()
-        # est_final_dist = self._estimate_final_distribution()
-        self.result = SimulationResultWrapper(paths)
+
+        final_dist = None
+        final_mat = None
+        if self._est_dist:
+            final_mat = self._estimate_final_transition_matrix(trials=1000)
+            final_dist = self.initial_distribution @ final_mat
+        self.result = SimulationResultWrapper(paths, final_dist, final_mat)
 
         if show_plots:
             self._show_plots()
@@ -82,21 +90,21 @@ class Simulation:
     def estimate_distribution_from_initial_state(
         self,
         initial_state: int,
-        final_epoch: int,
-        trials: int = 1_000,
+        epochs: int,
+        n_trials: int = 1_000,
     ) -> np.ndarray:
         """
-        Estimates the distribution after `final_epochs` epochs starting from `initial_state` by running the simulation `trials` times
+        Estimates the distribution after `epochs` epochs starting from `initial_state` by running the simulation `n_trials` times.
         """
         counts = np.zeros((self.n_states,))
-        for _ in range(trials):
+        for _ in range(n_trials):
             state = initial_state
-            for _ in range(1, final_epoch):
-                state = np.random.choice(
+            for _ in range(1, epochs):
+                state = self._rng.choice(
                     self.state_space, p=self.transition_matrix[state]
                 )
             counts[state] += 1
-        return counts / counts.sum()
+        return counts / n_trials
 
     def _print_parameters(self) -> None:
         print(
@@ -121,8 +129,10 @@ class Simulation:
 
             plt.xlabel("Step")
             plt.ylabel("State")
-            plt.xticks(list(range(0, self.steps, self.steps // 10)))
-            plt.yticks(list(range(0, self.n_states, self.n_states // 10)))
+            xtick_step = 1 if self.steps <= 20 else self.steps // 10
+            ytick_step = 1 if self.n_states <= 20 else self.n_states // 10
+            plt.xticks(list(range(0, self.steps, xtick_step)))
+            plt.yticks(list(range(0, self.n_states, ytick_step)))
             plt.ylim(bottom=-1, top=len(self.state_space))
             plt.title("Markov Chain Simulation")
             plt.tight_layout()
@@ -132,11 +142,11 @@ class Simulation:
         paths = [[] for _ in range(self.trials)]
         for i in range(self.trials):
             path = [0 for _ in range(self.steps)]
-            in_state = np.random.choice(self.state_space, p=self.initial_distribution)
+            in_state = self._rng.choice(self.state_space, p=self.initial_distribution)
             path[0] = in_state
             pv_state = in_state
             for j in range(1, self.steps):
-                cr_state = np.random.choice(
+                cr_state = self._rng.choice(
                     self.state_space, p=self.transition_matrix[pv_state]
                 )
                 path[j] = cr_state
@@ -144,8 +154,8 @@ class Simulation:
             paths[i] = path
         return paths
 
-    def _estimate_final_distribution(self, trials: int = 1_000) -> np.ndarray:
-        return np.concatenate(
+    def _estimate_final_transition_matrix(self, trials: int = 1_000) -> np.ndarray:
+        return np.stack(
             [
                 self.estimate_distribution_from_initial_state(i, self.steps, trials)
                 for i in range(self.n_states)